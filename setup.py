73a1c0120f4c6e6d34e67dc5dbe36012c2278f67
Manan Lad <65852934+luckyCasualGuy@users.noreply.github.com>
2025-05-22 11:54:34 +0530

    build updated

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'Django Rest Framework Extended Functionalities'
 LONG_DESCRIPTION = 'This package is very opinionated'
 
